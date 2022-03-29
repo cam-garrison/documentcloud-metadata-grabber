1fa504229a2dec14e34cc1d07b71127dc7f4418f
Cameron Garrison <cgarriso@bu.edu>
2022-03-29 13:28:58 -0400

    update doccloud version + update document accessing

--- main.py
+++ main.py
@@ -66,8 +66,9 @@ class MetaDataScrape(AddOn):
 
         # retrieve metadata information from each document.
         if self.documents:
-            for doc_id in self.documents:
-                document = self.client.documents.get(doc_id)
+            print(self.documents)
+            print(self.client.documents.list)
+            for document in self.client.documents.list(id__in=self.documents):
                 # set the metadata
                 metadata_list.append(set_data(document, []))
         elif self.query:
